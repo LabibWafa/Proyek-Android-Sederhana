948984e9ed1eb4f88b72c926d02401abfea01785
Labib <111201912229@mhs.dinus.ac.id>
2022-10-12 16:54:17 +0700

    Penambahan button toast

--- app/src/main/java/com/example/proyeksederhana/MainActivity.java
+++ app/src/main/java/com/example/proyeksederhana/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.proyeksederhana;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnNotif = findViewById(R.id.btn_notif):
+        btnNotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText( MainActivity.this, "Selamat Datang Di Android" , Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
